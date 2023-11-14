131260c2f0f9f04e524c2ebe1c9cb9d68899fadd
Nmn200 <n9462895233@gmail.com>
2023-11-14 15:51:44 +0530

    payment page made

--- src/pages/payment/index.jsx
+++ src/pages/payment/index.jsx
@@ -1,11 +1,110 @@
 import "./style.scss"
+import { useState } from 'react';
+
 
 const Payment = () => {
+  const [payments, setPayments] = useState([
+    {
+      id: '12354',
+      service: 'Group session',
+      date: '24 May 2024',
+      payment: '$1,000',
+      status: 'Delivered' // Corrected the typo here
+    },
+    {
+      id: '12354',
+      service: 'Group session',
+      date: '24 May 2024',
+      payment: '$1,000',
+      status: 'Pending'
+    },
+    {
+      id: '12354',
+      service: 'Group session',
+      date: '24 May 2024',
+      payment: '$1,000',
+      status: 'Pending'
+    },
+    {
+      id: '12354',
+      service: 'Group session',
+      date: '24 May 2024',
+      payment: '$1,000',
+      status: 'Cancelled'
+    },
+    {
+      id: '12354',
+      service: 'Group session',
+      date: '24 May 2024',
+      payment: '$1,000',
+      status: 'Cancelled'
+    },
+    {
+      id: '12354',
+      service: 'Group session',
+      date: '24 May 2024',
+      payment: '$1,000',
+      status: 'Cancelled'
+    },
+    {
+      id: '12354',
+      service: 'Group session',
+      date: '24 May 2024',
+      payment: '$1,000',
+      status: 'Cancelled'
+    },
+    {
+      id: '12354',
+      service: 'Group session',
+      date: '24 May 2024',
+      payment: '$1,000',
+      status: 'Cancelled'
+    },
+    {
+      id: '12354',
+      service: 'Group session',
+      date: '24 May 2024',
+      payment: '$1,000',
+      status: 'Cancelled'
+    },
+    {
+      id: '12354',
+      service: 'Group session',
+      date: '24 May 2024',
+      payment: '$1,000',
+      status: 'Cancelled'
+    }
+  ]);
+  
   return (
     <div className="payment">
-      <div className="paymentContainer">
-        This is payments
+      <div className='RecentPayments-container'>
+      <div className="top">
+        <h1>All Payments</h1>
+       
       </div>
+      <div className="table">
+        <div className="row">
+          <div className="col"><h4>ID</h4></div>
+          <div className="col"><h4>SERVICE</h4></div>
+          <div className="col"><h4>DATE</h4></div>
+          <div className="col"><h4>PAYMENT</h4></div>
+          <div className="col"><h4>STATUS</h4></div>
+        </div>
+        {payments.map((payment, i) => (
+          <div className='row' key={i}>
+            <div className='col'>{payment.id}</div>
+            <div className='col'>{payment.service}</div>
+            <div className='col'>{payment.date}</div>
+            <div className='col'>{payment.payment}</div>
+            <div className={`col ${payment.status === 'Cancelled' ? 'red' :
+              payment.status === 'Delivered' ? 'green' :
+                payment.status === 'Pending' ? 'blue' : ''
+              }`}>{payment.status}</div>
+          </div>
+        ))}
+      </div>
+    </div>
     </div>
   )
 }
