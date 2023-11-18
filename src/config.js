b8b9aac742f98dfc8d401afbe26498f057a2a76a
gunjan <gunjan.soral@gmail.com>
2023-11-18 21:32:43 +0530

    backend url fixed

--- src/config.js
+++ src/config.js
@@ -1,5 +1,5 @@
 module.exports = {
   frontend_url: 'http://localhost:3000',
   // backend_url: 'http://localhost:9000/counsellor'
-  backend_url: 'http://13.127.104.198:9000/counsellor'
+  backend_url: 'https://server.sortmycollege.com/counsellor'
 }
