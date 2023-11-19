d51b05c1dae69ab9bf3b0473fb4f45173ffb9752
gunjan <gunjan.soral@gmail.com>
2023-11-19 13:38:50 +0530

    frontend url updated

--- src/config.js
+++ src/config.js
@@ -1,5 +1,5 @@
 module.exports = {
-  frontend_url: 'https://sort-my-college-counsellor-panel-pmk4iipir-wepitch.vercel.app',
+  frontend_url: 'https://sort-my-college-counsellor-panel-three.vercel.app',
   // backend_url: 'http://localhost:9000/counsellor'
   backend_url: 'https://server.sortmycollege.com/counsellor'
 }
