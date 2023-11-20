a601c5e1acf59bf6a36d141cac48ac1a11d42ec4
gunjan <gunjan.soral@gmail.com>
2023-11-20 11:44:28 +0530

    google authentication production done

--- src/config.js
+++ src/config.js
@@ -1,5 +1,5 @@
 module.exports = {
   frontend_url: 'https://counsellor.sortmycollege.com',
-  // backend_url: 'http://localhost:9000/counsellor'
+  // backend_url: 'https://localhost:9000/counsellor',
   backend_url: 'https://server.sortmycollege.com/counsellor'
 }
