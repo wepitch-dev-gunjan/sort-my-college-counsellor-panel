c3a65cc888d18b5a7e8aa5eaeec24e8e62d0ae23
gunjan <gunjan.soral@gmail.com>
2023-11-21 17:09:33 +0530

    feedback pages desgn made

--- src/context/UserContext.js
+++ src/context/UserContext.js
@@ -5,19 +5,18 @@ import { useNavigate } from "react-router-dom"; // Assuming you are using React 
 export const UserContext = createContext();
 
 export const UserProvider = ({ children }) => {
-  const cookieValue = document.cookie;
-  console.log("cookies", cookieValue);
-  const [cookies] = useCookies(["token", "user"]);
   const navigate = useNavigate();
+  const [cookies, setCookie] = useCookies(["token", "user"]);
+
   // Initialize the user state with the token and isLoggedIn properties
   const [user, setUser] = useState({
     _id: cookies.user?._id || "",
     name: cookies.user?.name || "",
     email: cookies.user?.email || "",
     profile_pic: cookies.user?.profile_pic || "",
-    token: cookies?.token || "",
-    isLoggedIn: !!cookies.token,
-    // isLoggedIn: true,
+    token: cookies.token || "",
+    // isLoggedIn: !!cookies.token,
+    isLoggedIn: true,
   });
 
   // Redirect to login page if there is no token
