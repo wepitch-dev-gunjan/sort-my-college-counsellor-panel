7217bf6f74566a49d0b5705d81c6538c17a5409b
Nmn200 <n9462895233@gmail.com>
2023-11-21 17:24:20 +0530

    widget added

--- src/pages/dashboard/index.jsx
+++ src/pages/dashboard/index.jsx
@@ -1,30 +1,29 @@
-import { useContext } from 'react';
-import RecentPayments from '../../components/dashboardComponents/RecentPayments';
-import Summary from '../../components/dashboardComponents/summary';
-import Widget from '../../components/dashboardComponents/widget';
-import './style.scss'
-import { MediaQueryContext } from '../../context/MediaQueryContext';
+import { useContext } from "react";
+import RecentPayments from "../../components/dashboardComponents/RecentPayments";
+import Summary from "../../components/dashboardComponents/summary";
+import Widget from "../../components/dashboardComponents/widget";
+import "./style.scss";
+import { MediaQueryContext } from "../../context/MediaQueryContext";
 
 const Dashboard = () => {
   const { smallScreen } = useContext(MediaQueryContext);
   return (
     <div className="all-dashboard">
-      <div className='Dashboard-container'>
+      <div className="Dashboard-container">
         <div className="business-dashbaord">
           <h1>Business Dashboard</h1>
           <div className="widgets-container">
-            <Widget heading='USERS' value='1000' />
-            <Widget heading='INCOME' value='$10000' />
-            <Widget heading='SESSIONS' value='1000' />
+            <Widget heading="USERS" value="1000" />
+            <Widget heading="INCOME" value="$10000" />
+            <Widget heading="SESSIONS" value="1000" />
+            <Widget heading="REWARD POINTS" value="100" />
           </div>
         </div>
 
         {/* recent payments */}
         <RecentPayments />
       </div>
-      <div className="summary">
-        {!smallScreen && <Summary />}
-      </div>
+      <div className="summary">{!smallScreen && <Summary />}</div>
     </div>
   );
 };
