571f8d94a098d0d2da3b83f2fcb4f45951f2084b
Nmn200 <n9462895233@gmail.com>
2023-11-22 10:14:50 +0530

    profile page changes

---
+++ src/components/coverImage/index.jsx
@@ -0,0 +1,7 @@
+import "./style.scss";
+
+const CoverImage = () => {
+  return <div className="CoverImage-container">CoverImage</div>;
+};
+
+export default CoverImage;
--- src/pages/profile/Profile.jsx
+++ src/pages/profile/Profile.jsx
@@ -1,65 +1,58 @@
-import EditProfile from '../../components/editProfile';
-import './profile.scss'
-import { useState } from 'react';
-import ProfileImages from '../../components/profileImages/ProfileImages';
-import ProfilePic from '../../components/profilePic';
+import EditProfile from "../../components/editProfile";
+import "./profile.scss";
+import { useState } from "react";
+import ProfileImages from "../../components/profileImages/ProfileImages";
+import ProfilePic from "../../components/profilePic";
+import CoverImage from "../../components/coverImage";
 
 const Profile = () => {
-    // COde start for form 
-    const userProfile = {
-        "name": "John Doe",
-        "email": "johndoe@example.com",
-        "followers_count": 0,
-        "experience_in_years": 5,
-        "total_sessions_attended": 0,
-        "how_will_i_help": [],
-        "qualifications": [
-            "Sample Qualification 1",
-            "Sample Qualification 2"
-        ],
-        "languages_spoken": [
-            "English",
-            "Hindi"
-        ],
-        "location": {
-            "pin_code": 123456,
-            "city": "Sample City",
-            "state": "Sample State",
-            "country": "Sample Country"
-        },
-        "gender": "Male",
-        "age": null,
-        "client_testimonials": [],
-        "group_session_price": null,
-        "personal_session_price": null
-    };
+  // COde start for form
+  const userProfile = {
+    name: "John Doe",
+    email: "johndoe@example.com",
+    followers_count: 0,
+    experience_in_years: 5,
+    total_sessions_attended: 0,
+    how_will_i_help: [],
+    qualifications: ["Sample Qualification 1", "Sample Qualification 2"],
+    languages_spoken: ["English", "Hindi"],
+    location: {
+      pin_code: 123456,
+      city: "Sample City",
+      state: "Sample State",
+      country: "Sample Country",
+    },
+    gender: "Male",
+    age: null,
+    client_testimonials: [],
+    group_session_price: null,
+    personal_session_price: null,
+  };
 
-    console.log(userProfile);
+  console.log(userProfile);
 
-    const [isPopupOpen, setIsPopupOpen] = useState(false);
-    const openPopup = () => {
-        setIsPopupOpen(true);
-    };
+  const [isPopupOpen, setIsPopupOpen] = useState(false);
+  const openPopup = () => {
+    setIsPopupOpen(true);
+  };
 
-    const closePopup = () => {
-        setIsPopupOpen(false);
-    };
+  const closePopup = () => {
+    setIsPopupOpen(false);
+  };
 
-    // code end for form
+  // code end for form
 
-
-
-    return (
-        <div className="profile-container">
-            <ProfilePic />
-            {/* <ProfileImages /> */}
-            {/* <button onClick={openPopup}>Open Popup</button>
+  return (
+    <div className="profile-container">
+      <CoverImage />
+      <ProfilePic />
+      {/* <ProfileImages /> */}
+      {/* <button onClick={openPopup}>Open Popup</button>
             <button onClick={openPopup}>New Popup</button> */}
 
-            {/* <EditProfile isOpen={isPopupOpen} onClose={closePopup} /> */}
-        </div>
-    )
-}
+      {/* <EditProfile isOpen={isPopupOpen} onClose={closePopup} /> */}
+    </div>
+  );
+};
 
-
-export default Profile
+export default Profile;
