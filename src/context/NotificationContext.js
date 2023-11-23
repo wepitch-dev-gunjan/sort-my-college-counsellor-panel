c15590d8540d9b533c9ab4e357868ad57e8ed837
Nmn200 <n9462895233@gmail.com>
2023-11-23 09:57:56 +0530

    notification scroll

--- src/context/NotificationContext.js
+++ src/context/NotificationContext.js
@@ -6,21 +6,85 @@ export const NotificationProvider = ({ children }) => {
   const [notificationsEnable, setNotificationsEnable] = useState(true);
   const [notifications, setNotifications] = useState([
     {
+      _id: "12365452",
       title: "New Follower",
       message: "Naman followed you",
       read: false,
     },
     {
+      _id: "12365452",
       title: "New Follower",
       message: "Naman followed you",
       read: false,
     },
     {
+      _id: "12365452",
       title: "New Follower",
       message: "Naman followed you",
       read: false,
     },
     {
+      _id: "12365452",
+      title: "New Follower",
+      message: "Naman followed you",
+      read: false,
+    },
+    {
+      _id: "12365452",
+      title: "New Follower",
+      message: "Naman followed you",
+      read: false,
+    },
+    {
+      _id: "12365452",
+      title: "New Follower",
+      message: "Naman followed you",
+      read: false,
+    },
+    {
+      _id: "12365452",
+      title: "New Follower",
+      message: "Naman followed you",
+      read: false,
+    },
+    {
+      _id: "12365452",
+      title: "New Follower",
+      message: "Naman followed you",
+      read: false,
+    },
+    {
+      _id: "12365452",
+      title: "New Follower",
+      message: "Naman followed you",
+      read: false,
+    },
+    {
+      _id: "12365452",
+      title: "New Follower",
+      message: "Naman followed you",
+      read: false,
+    },
+    {
+      _id: "12365452",
+      title: "New Follower",
+      message: "Naman followed you",
+      read: false,
+    },
+    {
+      _id: "12365452",
+      title: "New Follower",
+      message: "Naman followed you",
+      read: false,
+    },
+    {
+      _id: "12365452",
+      title: "New Follower",
+      message: "Naman followed you",
+      read: false,
+    },
+    {
+      _id: "12365452",
       title: "New Follower",
       message: "Naman followed you",
       read: false,
