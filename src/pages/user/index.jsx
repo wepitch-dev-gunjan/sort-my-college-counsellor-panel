ae2b73a036a215f964c82a8dba55e97444706647
Nmn200 <n9462895233@gmail.com>
2023-11-27 09:54:38 +0530

    notification colour removed

--- src/pages/user/index.jsx
+++ src/pages/user/index.jsx
@@ -6,85 +6,49 @@ const Users = () => {
     {
       image: "https://www.w3schools.com/howto/img_avatar2.png",
       name: "Aman Sharma",
-      service: "Group session",
+      email: "abc@gmail.com",
       follow_date: "24 May 2024",
     },
     {
       image: "https://www.w3schools.com/howto/img_avatar2.png",
       name: "Aman Sharma",
-      service: "Group session",
+      email: "abc@gmail.com",
       follow_date: "24 May 2024",
     },
     {
       image: "https://www.w3schools.com/howto/img_avatar2.png",
       name: "Aman Sharma",
-      service: "Group session",
+      email: "abc@gmail.com",
       follow_date: "24 May 2024",
     },
     {
       image: "https://www.w3schools.com/howto/img_avatar2.png",
       name: "Aman Sharma",
-      service: "Group session",
+      email: "abc@gmail.com",
       follow_date: "24 May 2024",
     },
     {
       image: "https://www.w3schools.com/howto/img_avatar2.png",
       name: "Aman Sharma",
-      service: "Group session",
+      email: "abc@gmail.com",
       follow_date: "24 May 2024",
     },
     {
       image: "https://www.w3schools.com/howto/img_avatar2.png",
       name: "Aman Sharma",
-      service: "Group session",
+      email: "abc@gmail.com",
       follow_date: "24 May 2024",
     },
     {
       image: "https://www.w3schools.com/howto/img_avatar2.png",
       name: "Aman Sharma",
-      service: "Group session",
+      email: "abc@gmail.com",
       follow_date: "24 May 2024",
     },
     {
       image: "https://www.w3schools.com/howto/img_avatar2.png",
       name: "Aman Sharma",
-      service: "Group session",
-      follow_date: "24 May 2024",
-    },
-    {
-      image: "https://www.w3schools.com/howto/img_avatar2.png",
-      name: "Aman Sharma",
-      service: "Group session",
-      follow_date: "24 May 2024",
-    },
-    {
-      image: "https://www.w3schools.com/howto/img_avatar2.png",
-      name: "Aman Sharma",
-      service: "Group session",
-      follow_date: "24 May 2024",
-    },
-    {
-      image: "https://www.w3schools.com/howto/img_avatar2.png",
-      name: "Aman Sharma",
-      service: "Group session",
-      follow_date: "24 May 2024",
-    },
-    {
-      image: "https://www.w3schools.com/howto/img_avatar2.png",
-      name: "Aman Sharma",
-      service: "Group session",
-      follow_date: "24 May 2024",
-    },
-    {
-      image: "https://www.w3schools.com/howto/img_avatar2.png",
-      name: "Aman Sharma",
-      service: "Group session",
-      follow_date: "24 May 2024",
-    },
-    {
-      image: "https://www.w3schools.com/howto/img_avatar2.png",
-      name: "Aman Sharma",
-      service: "Group session",
+      email: "abc@gmail.com",
       follow_date: "24 May 2024",
     },
   ]);
@@ -100,7 +64,7 @@ const Users = () => {
             <h4>NAME</h4>
           </div>
           <div className="col">
-            <h4>SERVICE</h4>
+            <h4>EMAIL</h4>
           </div>
           <div className="col">
             <h4>FOLLOW DATE</h4>
@@ -116,7 +80,7 @@ const Users = () => {
               </div>
 
               <div className="col">{follower.name}</div>
-              <div className="col">{follower.service}</div>
+              <div className="col">{follower.email}</div>
               <div className="col">{follower.follow_date}</div>
             </div>
           ))}
