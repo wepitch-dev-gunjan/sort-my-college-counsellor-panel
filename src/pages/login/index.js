f40e8d6044b2b2db11b0e062b6b42e5873a561d6
gunjan <gunjan.soral@gmail.com>
2023-11-27 15:05:09 +0530

    gjgjgjgjggj

--- src/pages/login/index.js
+++ src/pages/login/index.js
@@ -3,16 +3,39 @@ import { FaGoogle, FaEye, FaEyeSlash } from "react-icons/fa";
 import "./style.scss";
 import Logo from "../../assets/logo.svg";
 import { TextField, InputAdornment, IconButton } from "@mui/material";
-import { Link } from "react-router-dom";
+import { Link, Navigate, useNavigate } from "react-router-dom";
 import { backend_url } from "../../config";
+import axios from "axios";
 
 const Login = () => {
   const [isLoading, setIsLoading] = useState(false);
   const [showPassword, setShowPassword] = useState(false);
-
-  const handleGoogleLogin = () => {
+  const navigate = useNavigate();
+  // const handleGoogleLogin = () => {
+  //   setIsLoading(true);
+  //   window.location.href = `${backend_url}/auth/google`;
+  // };
+  const handleGoogleLogin = async () => {
     setIsLoading(true);
-    window.location.href = `${backend_url}/auth/google`;
+
+    try {
+      // Make a request to your backend with Axios
+      const response = await axios.get(`${backend_url}/auth/google`, { withCredentials: true });
+
+      // Check if the response indicates successful login
+      if (response.data.success) {
+        // Redirect to a success page or home page
+        navigate("/dashboard");
+      } else {
+        // Handle login failure
+        // You might want to show an error message or perform other actions
+      }
+    } catch (error) {
+      // Handle errors
+      console.error("Error during Google login:", error);
+    } finally {
+      setIsLoading(false);
+    }
   };
 
   const togglePasswordVisibility = () => {
