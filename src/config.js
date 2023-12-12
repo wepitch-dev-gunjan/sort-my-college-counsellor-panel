eadb6999c1b8bb3dc3bd97d241fd3616c50da8c9
gunjan <gunjan.soral@gmail.com>
2023-12-12 12:49:29 +0530

    jhjhjhj

--- src/config.js
+++ src/config.js
@@ -1,8 +1,8 @@
 module.exports = {
   frontend_url: 'https://counsellor.sortmycollege.com',
   // backend_url: 'https://localhost:9000/counsellor',
-  backend_url: 'https://localhost:9000',
-  // backend_url: 'https://server.sortmycollege.com'
+  // backend_url: 'https://localhost:9000',
+  backend_url: 'https://server.sortmycollege.com'
 }
 
 // import axios from 'axios'
