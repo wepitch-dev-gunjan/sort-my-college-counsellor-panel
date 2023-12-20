13d42bf8a0ce0396d10c079453b806d1fe4911c8
Nmn200 <n9462895233@gmail.com>
2023-12-20 13:29:23 +0530

    ghghgh

--- src/components/filters/index.jsx
+++ src/components/filters/index.jsx
@@ -1,10 +1,12 @@
 import React, { useState } from 'react';
-import './style.scss'
+import './style.scss';
 import { Box, Slider } from '@mui/material';
+import { FaIndianRupeeSign } from "react-icons/fa6";
 
 const Filters = () => {
-  const [selectedType, setSelectedType] = useState(''); // State to track the selected type
-  const [selectedStatus, setSelectedStatus] = useState(''); // State to track the selected type
+  const [selectedType, setSelectedType] = useState('');
+  const [selectedStatus, setSelectedStatus] = useState('');
+  const [sessionFee, setSessionFee] = useState([0, 0]);
 
   const handleTypeChange = (event) => {
     setSelectedType(event.target.value);
@@ -24,88 +26,100 @@ const Filters = () => {
       label: '500',
     },
     {
-        value: 1000,
-        label: '1000',
-      },
-      {
-        value: 1500,
-        label: '1500',
-      },
-      {
-        value: 2000,
-        label: '2000',
-      },
+      value: 1000,
+      label: '1000',
+    },
+    {
+      value: 1500,
+      label: '1500',
+    },
+    {
+      value: 2000,
+      label: '2000',
+    },
   ];
-  
+
   function valuetxt(value) {
     return `${value}`;
   }
 
   const marks = [
     {
-      value: 0,
-      label: '0',
-    },
-    {
-      value: 15,
-      label: '15 min',
-    },
-    {
-      value: 30,
-      label: '30 min ',
+      value: 45,
+      label: '45m ',
     },
     {
       value: 60,
-      label: '60 min ',
+      label: '60m ',
     },
     {
-        value: 90,
-        label: '90 min',
+      value: 75,
+      label: '75m',
     },
     {
-        value: 120,
-        label: '120 min',
-        
+      value: 90,
+      label: '90m',
+    },
+    {
+      value: 105,
+      label: '105m',
+    },
+    {
+      value: 120,
+      label: '120m',
     },
   ];
-  
+
   function valuetext(value) {
-    return `${value} min`;
+    return `${value}m`;
   }
 
-
-  return (  
-
+  return (
     <div className="filter-container">
       <div className="type">
+        <p>Session Type</p>
         <select value={selectedType} onChange={handleTypeChange}>
-          <option value="">Select session Type</option>
           <option value="personal">Personal</option>
           <option value="group">Group</option>
+          <option value="">All</option>
         </select>
       </div>
       <div className="fees">
         <p>Session Fee</p>
         <Box sx={{ width: 200 }}>
-        <Slider
-        aria-label="Fees"
-        getAriaValueText={valuetxt}
-        marks={feeMarks}
-        />
+          <Slider
+            value={sessionFee}
+            onChange={(e) => setSessionFee(e.target.value)}
+            min={200}
+            max={5000}
+            step={100}
+          />
+          <div className="values">
+            {sessionFee.map((element, i) => (
+                <span key={i}><FaIndianRupeeSign />{element}</span>
+            ))}
+          </div>
         </Box>
+      </div>
+      <div className="date-range">
+
       </div>
       <div className="duration">
         <p>Session duration</p>
-        <Box sx={{ width: 320 }}>
-           <Slider
-              aria-label="Duration"
-              defaultValue={60}
-              getAriaValueText={valuetext}
-              marks={marks}
-           />
+        <Box sx={{ width: 250 }}>
+          <Slider
+            aria-label="Duration"
+            defaultValue={45} // Set the starting point to 45 minutes
+            getAriaValueText={valuetext}
+            step={null}
+            marks={marks}
+            min={45}
+            max={120}
+          />
         </Box>
       </div>
       <div className="status">
+        <p>Session Status</p>
         <select value={selectedStatus} onChange={handleStatusChange}>
           <option value="">Select status</option>
           <option value="personal">Available</option>
