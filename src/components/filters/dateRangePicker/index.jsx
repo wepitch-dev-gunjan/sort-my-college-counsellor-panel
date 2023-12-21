ed10dfd7be7b2476e47ae8502c7946f4e67baaa3
gunjan <gunjan.soral@gmail.com>
2023-12-21 10:31:52 +0530

    kfkfkfkfk

--- src/components/filters/dateRangePicker/index.jsx
+++ src/components/filters/dateRangePicker/index.jsx
@@ -1,30 +1,39 @@
-import { useState } from 'react';
-import './style.scss'
+import React, { useState } from 'react';
+import './style.scss';
 import { DatePicker } from '@mui/x-date-pickers';
 import dayjs from 'dayjs';
+import 'dayjs/locale/en'; // Import the locale if not already done
 
 const DateRangePicker = () => {
   const [dates, setDates] = useState({
-    startDate: dayjs('2023-04-17'),
-    endDate: dayjs('2023-04-17'),
+    startDate: '2023-04-17',
+    endDate: '2023-04-17',
   });
+
+  const handleStartDateChange = (date) => {
+    setDates({ ...dates, startDate: date.format('YYYY-MM-DD') });
+  };
+
+  const handleEndDateChange = (date) => {
+    setDates({ ...dates, endDate: date.format('YYYY-MM-DD') });
+  };
+
   return (
-    <div className='DateRangePicker-container'>
+    <div className="DateRangePicker-container">
       <div className="start-date">
         <DatePicker
-          label="Controlled picker"
-          value={dates.startDate}
-          onChange={(value) => setDates({ ...dates, startDate: value })}
+          label="Start Date"
+          value={dayjs(dates.startDate)}
+          onChange={(value) => handleStartDateChange(value)}
         />
       </div>
       -
       <div className="end-date">
         <DatePicker
-          label="Controlled picker"
-          value={dates.endDate}
-          onChange={(value) => setDates({ ...dates, endDate: value })}
+          label="End Date"
+          value={dayjs(dates.endDate)}
+          onChange={(value) => handleEndDateChange(value)}
         />
-        {dates.endDate}
       </div>
     </div>
   );
