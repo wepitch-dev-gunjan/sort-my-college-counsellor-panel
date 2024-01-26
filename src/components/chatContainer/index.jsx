e08a91f56f80b9329106856f623bee538ab5ab7c
Nmn200 <n9462895233@gmail.com>
2024-01-26 12:36:26 +0530

    skjdhgksdjfg

---
+++ src/components/chatContainer/index.jsx
@@ -0,0 +1,17 @@
+import React from 'react'
+import './style.scss'
+
+const ChatContainer = () => {
+  return (
+    <div className='ChatContainer-container'>
+      <div className="main-container">
+
+      </div>
+      <div className="input-container">
+        input
+      </div>
+    </div>
+  )
+}
+
+export default ChatContainer
--- src/pages/help/index.jsx
+++ src/pages/help/index.jsx
@@ -1,8 +1,12 @@
-import { Autocomplete, Stack, TextField } from '@mui/material';
+import { Autocomplete, Stack, TextField, Tooltip } from '@mui/material';
 import './style.scss';
 import { MdOutlineChat } from "react-icons/md";
+import { useState } from 'react';
+import ChatContainer from '../../components/chatContainer';
 
 const Help = () => {
+
+  const [chatEnable, setChatEnable] = useState(false);
   const top100Films = [
     { title: 'The Shawshank Redemption', year: 1994 },
     { title: 'The Godfather', year: 1972 },
@@ -10,6 +14,7 @@ const Help = () => {
   ];
   return (
     <div className='Help-container'>
+      {chatEnable && <ChatContainer />}
       <div className="help-search">
         <h1>Welcome! How can we help?</h1>
         <Stack spacing={2} sx={{ width: 300 }}>
@@ -32,9 +37,11 @@ const Help = () => {
       />
     </Stack>
       </div>
-      <div className="chat-help">
+      <Tooltip title="Chat" placement='left'>
+      <div className="chat-help" onClick={() => setChatEnable(prev => !prev)}>
         <MdOutlineChat size={32} color='white'/>
       </div>
+      </Tooltip>
     </div>
   );
 };
