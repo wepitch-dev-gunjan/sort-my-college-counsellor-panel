a81403bc0fd5b7a30434a29eac49d7a93f8658a8
Nmn200 <n9462895233@gmail.com>
2024-01-26 13:18:25 +0530

    chat box

---
+++ src/components/chatBox/index.jsx
@@ -0,0 +1,16 @@
+import React from 'react'
+import './style.scss'
+
+const ChatBox = (text) => {
+    
+  return (
+    <div className='ChatBox-container'>
+      <p>
+        {/* {text} */}
+        chatting...
+        </p>
+    </div>
+  )
+}
+
+export default ChatBox
--- src/components/chatContainer/index.jsx
+++ src/components/chatContainer/index.jsx
@@ -1,12 +1,13 @@
 import React from 'react'
 import './style.scss'
 import ChatInput from './chatInput'
+import ChatBox from '../chatBox'
 
 const ChatContainer = () => {
   return (
     <div className='ChatContainer-container'>
       <div className="main-container">
-
+        <ChatBox />
       </div>
       <div className="input-container">
         <ChatInput onSend={(input) => console.log(input)} />
