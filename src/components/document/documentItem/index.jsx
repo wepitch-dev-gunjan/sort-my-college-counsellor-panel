2617b69608780163b507a743067dec0d67dd5ebe
gunjan <gunjan.soral@gmail.com>
2024-02-27 17:13:50 +0530

    sdfsdf

--- src/components/document/documentItem/index.jsx
+++ src/components/document/documentItem/index.jsx
@@ -1,31 +1,82 @@
-import React, { useRef } from "react";
+import React, { useContext, useRef, useState } from "react";
 import { MdDeleteOutline } from "react-icons/md";
 import { Tooltip } from "@mui/material";
+import { MdDeleteForever } from "react-icons/md";
 import "./style.scss";
+import { backend_url } from "../../../config";
+import axios from "axios";
+import { toast } from "react-toastify";
+import { UserContext } from "../../../context/UserContext";
 
 const DocumentItem = ({
-  index,
   document,
-  onDocumentChange,
-  onFieldChange,
-  onDelete,
+  documentTypes,
+  documentType,
+  file,
   editProfileEnable,
+  getIdByName,
+  getDocumentTypeFromId
 }) => {
-  const handleFileChange = (e) => {
-    const file = e.target.files[0];
-    onDocumentChange(index, file);
-  };
-
   const hiddenFileInput = useRef(null);
+  const { user } = useContext(UserContext);
+  const [currentField, setCurrentField] = useState(documentType);
+
+  console.log("document types in document item", documentTypes)
+
+
+  const handleSelect = (e) => {
+    console.log(document)
+    setCurrentField(e.target.value);
+  }
 
   const handleClick = () => {
     hiddenFileInput.current.click();
   };
 
-  const handleFieldChange = (e) => {
-    const selectedField = e.target.value;
-    onFieldChange(index, selectedField);
+  const handleFileChange = async (e) => {
+    try {
+      const file = e.target.files[0];
+      const formData = new FormData();
+      formData.append("file", file);
+
+      const { data } = await axios.post(
+        `${backend_url}/counsellor/document/upload-document`,
+        formData,
+        {
+          headers: {
+            Authorization: user.token // Assuming user.token contains the authorization token
+          },
+          params: {
+            document_type: getIdByName(currentField) // Assuming currentField is defined elsewhere in your code
+          }
+        }
+      );
+
+      // Update state or do something with the response data if needed
+      console.log("Upload successful:", data);
+      toast.success("Document uploaded successfully"); // Assuming toast is defined and comes from a library like react-toastify
+
+    } catch (error) {
+      // Improve error handling
+      console.error("Error uploading document:", error.response ? error.response.data : error.message);
+      toast.error("Error uploading document. Please try again later.");
+    }
   };
+
+  const handleDeleteDocument = async () => {
+    try {
+      const { data } = await axios.delete(`${backend_url}/counsellor/document/${document._id}`,
+        null,
+        {
+          headers: {
+            Authorization: user.token
+          }
+        })
+
+    } catch (error) {
+      console.log(error)
+    }
+  }
 
   return (
     <div className="DocumentItem">
@@ -33,32 +84,49 @@ const DocumentItem = ({
       <div className="row DocumentItemRow">
         <div className="col DocumentItemCol">
           <div className="dropdown">
-            <select
-              onChange={handleFieldChange}
-              value={document?.selectedField}
-            >
-              <option value="field1">Career Counselling Certificate</option>
-              <option value="field2">Experience Certificate</option>
-              <option value="field3">Proof of working</option>
-              <option value="field4">
-                Proof if you have your own counselling company
-              </option>
-              <option value="field5">Degree of Bachelor in Counselling</option>
-            </select>
+            {editProfileEnable && !documentType ? (
+              <select
+                onChange={handleSelect}
+                value={currentField}
+              >
+                {documentTypes.map(dType => (
+                  <option value={dType.name} key={dType._id} >{dType.name}</option>
+                ))}
+              </select>
+            ) : (
+              <>
+                <p>{getDocumentTypeFromId(documentType)}</p>
+              </>
+            )}
+
           </div>
-          <div className="upload">
-            <button className="button-upload" onClick={handleClick}>
-              Upload a file
-            </button>
-            <input
-              className="upload-btn"
-              type="file"
-              onChange={handleFileChange}
-              accept=".pdf"
-              ref={hiddenFileInput}
-              style={{ display: "none" }}
-            />
-            {editProfileEnable && (
+
+          {editProfileEnable ? (
+            <>
+              <button className="button-upload" onClick={handleClick}>
+                Upload a file
+              </button>
+              <input
+                className="upload-btn"
+                type="file"
+                onChange={handleFileChange}
+                accept=".pdf"
+                ref={hiddenFileInput}
+                style={{ display: "none" }}
+              />
+              <div className="delete-button" onClick={handleDeleteDocument}>
+                <MdDeleteForever size="20" />
+              </div>
+            </>
+          ) : (
+            <div >
+              <a href={file} target="_blank" >link</a>
+            </div>
+          )}
+
+          {/* <div className="upload">
+            
+            {/* {editProfileEnable && (
               <div className="up-icons">
                 <Tooltip title="Delete" placement="bottom">
                   <div
@@ -71,7 +139,7 @@ const DocumentItem = ({
                 </Tooltip>
               </div>
             )}
-          </div>
+          </div> */}
         </div>
       </div>
       {/* changed by r  */}
--- src/components/document/index.jsx
+++ src/components/document/index.jsx
@@ -8,140 +8,126 @@ import { backend_url } from "../../config";
 import { UserContext } from "../../context/UserContext";
 
 const Documents = ({
-  index,
-  profile,
-  setProfile,
   editProfileEnable,
-  onDelete,
-  // onDocumentChange,
 }) => {
-  const [documents, setDocuments] = useState([
-    {
-      selectedField: "",
-    },
-  ]);
+  const { user } = useContext(UserContext)
+  const [documents, setDocuments] = useState([]);
+  const [documentTypes, setDocumentTypes] = useState([]);
+  const [filteredDocumentTypes, setFilteredDocumentTypes] = useState([]);
+  // const [remainingDocuments, setRemainingDocuments] = useState(documentTypes)
 
-  const [uploadedFiles, setUploadedFiles] = useState([]);
-  const [addDocumentClickCount, setAddDocumentClickCount] = useState(0);
-  const {user}=useContext(UserContext)
-
-
-  
-  const handleFileChange = async (e) => {
-    const file = e.target.files[0];
-  
-    // Validate file type
-    const allowedTypes = ["application/pdf", "image/jpeg", "image/jpg", "image/png"];
-    if (file && !allowedTypes.includes(file.type)) {
-      console.log("Invalid file type. Please upload a PDF, JPEG, JPG, or PNG file.");
-      return;
+  // fetch documents and store it to the documents state
+  const getDocuments = async () => {
+    try {
+      const { data } = await axios.get(`${backend_url}/counsellor/document/get-documents`, {
+        headers: {
+          Authorization: user.token
+        }
+      });
+      console.log(data)
+      setDocuments(data)
+    } catch (error) {
+      console.log(error);
+      toast("Error getting documents")
     }
-  
-    // Validate file size
-    const maxSizeInBytes = 1 * 1024 * 1024; 
-    if (file && file.size > maxSizeInBytes) {
-      toast("File size exceeds the limit. Please upload a smaller file than 1Mb.");
-      return;
+  }
+  // fetch documentTypes and store it to documentTypes state
+  const getDocumentTypes = async () => {
+    try {
+      const { data } = await axios.get(`${backend_url}/counsellor/documentType/documentTypes`);
+      console.log("asdfasdf", data, documents)
+      setDocumentTypes(data)
+    } catch (error) {
+      console.log(error);
+      toast("Error getting documents")
     }
-  
-    // onDocumentChange(index, file);
+  }
 
-      // Update the uploaded files state
-      setUploadedFiles([...uploadedFiles, file]); 
-
-      try {
-        const payload={file}
-        console.log(payload);
-        const {data}= await axios.post(`${backend_url}/counsellor/document/upload-document`,payload, {
-          headers:{
-            Authorization: user.token
-          }
-        })
-        
-      } catch (error) {
-        console.log(error);
-        
-      }
-      
-
-  };
-
-  
+  const getFilteredDocumentTypes = (dTypes) => {
+    const filteredTypesFromDocuments = documents.map(document => document.document_type)
+    return dTypes.filter(documentType => !filteredTypesFromDocuments.includes(documentType._id))
+  }
 
   const handleAddDocument = () => {
-    setAddDocumentClickCount(addDocumentClickCount + 1);
-    setDocuments([...documents, { file: null, selectedField: "" }]);
-  };
+    setDocuments([...documents, {
+      user: user._id,
+      _id: user._id,
+      document_type: ""
+    }])
+  }
 
-  const handleDeleteDocument = async (index) => {
-    const updatedDocuments = [...documents];
-    updatedDocuments.splice(index, 1);
-    setDocuments(updatedDocuments);
-   
-
-  };
-
-
-  const hiddenFileInput = useRef(null);
-
-  const handleClick = () => {
-    hiddenFileInput.current.click();
-  };
-
-  const handleDocumentChange = (index, file) => {
-    const updatedDocuments = [...documents];
-    updatedDocuments[index].file = file;
-    setDocuments(updatedDocuments);
-  };
-
-  const handleFieldChange = (index, selectedField) => {
-    const updatedDocuments = [...documents];
-    updatedDocuments[index].selectedField = selectedField;
-    setDocuments(updatedDocuments);
-  };
-
-  const handleUpload = async () => {
-    // Implement Axios request to upload documents to the server
-    // Use FormData to send the files to the server
-    const formData = new FormData();
-    documents.forEach((document) => {
-      formData.append("files", document.file);
-      formData.append("fields", document.selectedField);
-    });
-
-    try {
-      // const response = await axios.post("your-upload-endpoint", formData);
-      // console.log("Upload success:", response.data);
-    } catch (error) {
-      // Handle the error
-      console.error("Upload failed:", error);
+  useEffect(() => {
+    const promises = async () => {
+      await getDocuments()
+      await getDocumentTypes()
+      console.log("ho hi nhi rha hai")
     }
-  };
+    promises()
+    // setRemainingDocuments(documents)
 
+  }, [user])
+
+  // useEffect(() => {
+  //   console.log("filtered document types", filteredDocumentTypes())
+  // }, [documentTypes])
+
+  function getIdByName(name) {
+    for (let i = 0; i < documentTypes.length; i++) {
+      if (documentTypes[i].name === name) {
+        return documentTypes[i]._id;
+      }
+    }
+    // Return null if no match found
+    return null;
+  }
+
+  const getDocumentTypeFromId = (id) => {
+    const documentTypeName = documentTypes.filter(documentType => {
+      return documentType._id === id;
+    });
+    return documentTypeName[0]?.name;
+  }
+  useEffect(() => {
+    setFilteredDocumentTypes(getFilteredDocumentTypes(documentTypes))
+  }, [documentTypes])
   return (
     <div className="Documents-container">
       <div className="heading">
         <h2>Documents</h2>
       </div>
-      <DocumentItem
-        index={0}
-        document={documents[0]}
-        onDocumentChange={handleDocumentChange}
-        onFieldChange={handleFieldChange}
-        onDelete={handleDeleteDocument}
-      />
-      {documents.slice(1).map((document, index) => (
+
+      {/* {documents.filter(document => document?.required).map(document => (
         <DocumentItem
-          key={index + 1}
-          index={index + 1}
           document={document}
-          onDocumentChange={handleDocumentChange}
-          onFieldChange={handleFieldChange}
-          onDelete={handleDeleteDocument}
+          key={document._id}
+          documentTypes={documentTypes}
+          documentType={document.document_type}
+          file={document.file}
           editProfileEnable={editProfileEnable}
         />
+      ))} */}
+
+      {documents.map(document => (
+        <DocumentItem
+          document={document}
+          key={document._id}
+          documentTypes={filteredDocumentTypes}
+          documentType={document.document_type}
+          file={document.file}
+          editProfileEnable={editProfileEnable}
+          getIdByName={getIdByName}
+          getDocumentTypeFromId={getDocumentTypeFromId}
+        />
       ))}
-      {editProfileEnable && addDocumentClickCount < 4 && (
+
+      {editProfileEnable && (
+        <div className="add-document"
+          onClick={handleAddDocument}
+        >
+          <span>Add document</span>
+        </div>
+      )}
+      {/* {editProfileEnable && addDocumentClickCount < 4 && (
         <Tooltip
           title={
             <Typography style={{ fontSize: "16px" }}>
@@ -161,61 +147,80 @@ const Documents = ({
         >
           <button onClick={handleAddDocument}>+</button>
         </Tooltip>
-      )}
-      <div className="row">
-        <div className="col">
-          <div className="info-field">
-            <p>Aadhar Card*</p>
-          </div>
-          <div className="upload">
-            {uploadedFiles[index] ? (
-              <div>
-                <p>{uploadedFiles[index].name}</p>
-                <button onClick={() => handleDeleteDocument(index)}>
-                  Remove
-                </button>
-              </div>
-            ) : (
-              <React.Fragment>
-                <button className="button-upload" onClick={handleClick}>
-                  Upload a file
-                </button>
-                <input
-                  className="upload-btn"
-                  type="file"
-                  onChange={handleFileChange}
-                  accept=".pdf, .jpg, .jpeg, .png"
-                  ref={hiddenFileInput}
-                  style={{ display: "none" }}
-                />
-              </React.Fragment>
-            )}
-          </div>
-        </div>
-      </div>
-      <div className="row">
-        <div className="col">
-          <div className="info-field">
-            <p>Pan Card*</p>
-          </div>
-          <div className="upload">
-            <button className="button-upload" onClick={handleClick}>
-              Upload a file
-            </button>
-            <input
-              className="upload-btn"
-              type="file"
-              onChange={handleFileChange}
-              accept=".pdf"
-              ref={hiddenFileInput}
-              style={{ display: "none" }}
-            />
-          </div>
-          
-        </div>
-      </div>
+      )} */}
     </div>
   );
 };
 
 export default Documents;
+
+
+// const handleFileChange = async (e) => {
+//   const file = e.target.files[0];
+
+//   // Validate file type
+//   const allowedTypes = ["application/pdf", "image/jpeg", "image/jpg", "image/png"];
+//   if (file && !allowedTypes.includes(file.type)) {
+//     console.log("Invalid file type. Please upload a PDF, JPEG, JPG, or PNG file.");
+//     return;
+//   }
+
+//   // Validate file size
+//   const maxSizeInBytes = 1 * 1024 * 1024;
+//   if (file && file.size > maxSizeInBytes) {
+//     toast("File size exceeds the limit. Please upload a smaller file than 1Mb.");
+//     return;
+//   }
+
+//   // onDocumentChange(index, file);
+
+//   // Update the uploaded files state
+//   setUploadedFiles([...uploadedFiles, file]);
+
+//   try {
+//     const payload = { file }
+//     console.log(payload);
+//     const { data } = await axios.post(`${backend_url}/counsellor/document/upload-document`, payload, {
+//       headers: {
+//         Authorization: user.token
+//       }
+//     })
+
+//   } catch (error) {
+//     console.log(error);
+
+//   }
+
+
+// };
+
+
+
+// const handleAddDocument = () => {
+//   setAddDocumentClickCount(addDocumentClickCount + 1);
+//   setDocuments([...documents, { file: null, selectedField: "" }]);
+// };
+
+// const handleDeleteDocument = async (index) => {
+//   const updatedDocuments = [...documents];
+//   updatedDocuments.splice(index, 1);
+//   setDocuments(updatedDocuments);
+// };
+
+// const handleUpload = async () => {
+//   // Implement Axios request to upload documents to the server
+//   // Use FormData to send the files to the server
+//   const formData = new FormData();
+//   documents.forEach((document) => {
+//     formData.append("files", document.file);
+//     formData.append("fields", document.selectedField);
+//   });
+
+//   try {
+//     // const response = await axios.post("your-upload-endpoint", formData);
+//     // console.log("Upload success:", response.data);
+//   } catch (error) {
+//     // Handle the error
+//     console.error("Upload failed:", error);
+//   }
+// };
