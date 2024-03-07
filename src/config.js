f4a0344c875d0634e3781e8eb94791179ffb24e2
gunjan <gunjan.soral@gmail.com>
2024-03-07 12:00:50 +0530

    asdfasdf

--- src/config.js
+++ src/config.js
@@ -5,10 +5,10 @@ module.exports = {
     NODE_ENV === "production"
       ? "https://counsellor.sortmycollege.com"
       : "https://localhost:3000",
-      // : "https://192.168.0.36:3000",
+  // : "https://192.168.0.36:3000",
   backend_url:
     NODE_ENV === "production"
-      ? "https://server.sortmycollege.com"
+      ? "https://sortmycollegeapp.com"
       : "http://localhost:9000",
-      // : "http://192.168.0.36:9000",
+  // : "http://192.168.0.36:9000",
 };
