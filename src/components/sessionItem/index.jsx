41b0e89d27e1512d04ce5217c9c4ab8bce56d290
Nikhil mandia <nikhilmandia7@gmail.com>
2024-03-29 14:48:29 +0530

    edit session removed

--- src/components/sessionItem/index.jsx
+++ src/components/sessionItem/index.jsx
@@ -1,18 +1,18 @@
-import React, { useContext, useRef, useState } from 'react';
-import axios from 'axios';
-import { CiMenuKebab } from 'react-icons/ci';
-import { AiOutlineDelete, AiOutlineEdit } from 'react-icons/ai';
+import React, { useContext, useRef, useState } from "react";
+import axios from "axios";
+import { CiMenuKebab } from "react-icons/ci";
+import { AiOutlineDelete, AiOutlineEdit } from "react-icons/ai";
 import "./session.scss";
-import { UserContext } from '../../context/UserContext';
-import config from '@/config';
-import useClickOutside from '../../customHooks/useClickOutside';
-import dayjs from 'dayjs';
-import { toast } from 'react-toastify';
-import './session.scss';
+import { UserContext } from "../../context/UserContext";
+import config from "@/config";
+import useClickOutside from "../../customHooks/useClickOutside";
+import dayjs from "dayjs";
+import { toast } from "react-toastify";
+import "./session.scss";
 const { backend_url } = config;
 
 const SessionCard = ({ session, setSessions, getResponse }) => {
-  const menuRef = useRef(null)
+  const menuRef = useRef(null);
   const { user } = useContext(UserContext);
   const [showMenu, setShowMenu] = useState(false);
   const [editMode, setEditMode] = useState(false);
@@ -23,23 +23,31 @@ const SessionCard = ({ session, setSessions, getResponse }) => {
   });
 
   const formatDate = (date) => {
-    return dayjs(date).format('YYYY-MM-DD');
+    return dayjs(date).format("YYYY-MM-DD");
   };
 
-  const handleSave = async () => {
+  const handleSave = async (e) => {
+    e.preventDefault();
     try {
-      const response = await axios.put(`${backend_url}/counsellor/sessions/${sessionDetails._id}`, sessionDetails, {
-        headers: {
-          Authorization: user.token
+      const response = await axios.put(
+        `${backend_url}/counsellor/sessions/${sessionDetails._id}`,
+        sessionDetails,
+        {
+          headers: {
+            Authorization: user.token,
+          },
         }
-      });
-      console.log('Session updated successfully', response.data);
+      );
+      console.log("Session updated successfully", response.data);
       setEditMode(false);
-      setSessions(prev => prev.map(item => (item._id === sessionDetails._id ? item : response.data.session)));
+      setSessions((prev) =>
+        prev.map((item) =>
+          item._id === sessionDetails._id ? item : response.data.session
+        )
+      );
       // Perform any state updates or re-fetch the session list as needed
-
     } catch (error) {
-      console.error('An error occurred:', error);
+      console.error("An error occurred:", error);
     }
   };
 
@@ -50,49 +58,57 @@ const SessionCard = ({ session, setSessions, getResponse }) => {
 
   const handleJoinNow = () => {
     if (session.session_link) {
-      window.open(session.session_link, '_blank');
+      window.open(session.session_link, "_blank");
     }
   };
 
   const handleDelete = async () => {
     try {
       // Send an axios request to the server to delete the session
-      const { data } = await axios.delete(`${backend_url}/counsellor/sessions/${session._id}`, {
-        headers: {
-          Authorization: user.token
+      const { data } = await axios.delete(
+        `${backend_url}/counsellor/sessions/${session._id}`,
+        {
+          headers: {
+            Authorization: user.token,
+          },
         }
-      });
+      );
       toast(data.message);
       getResponse();
     } catch (error) {
       toast(error.message);
-      console.error('An error occurred:', error);
+      console.error("An error occurred:", error);
     }
   };
 
   return (
     <div className="sessions-i">
       <div className="top">
-        <h2>{session.session_type + ' Session'}</h2>
-        <div className="drop-down-button" onClick={() => setShowMenu(true)} >
+        <h2>{session.session_type + " Session"}</h2>
+        <div className="drop-down-button" onClick={() => setShowMenu(true)}>
           <CiMenuKebab />
         </div>
       </div>
 
-      {<div ref={menuRef} className={`${showMenu && 'display-active'} drop-down-menu`}>
-        <div onClick={() => setEditMode(true)} className="menu-item">
-          <AiOutlineEdit />
-          <span>Edit</span>
+      {
+        <div
+          ref={menuRef}
+          className={`${showMenu && "display-active"} drop-down-menu`}
+        >
+          {/* <div onClick={() => setEditMode(true)} className="menu-item">
+            <AiOutlineEdit />
+            <span>Edit</span>
+          </div> */}
+          <div onClick={handleDelete} className="menu-item">
+            <AiOutlineDelete />
+            <span>Delete</span>
+          </div>
         </div>
-        <div onClick={handleDelete} className="menu-item">
-          <AiOutlineDelete />
-          <span>Delete</span>
-        </div>
-      </div>}
+      }
 
       {editMode ? (
-        <form className='edit-mode-form' onSubmit={handleSave}>
-          <table className='edit-mode-table'>
+        <form className="edit-mode-form" onSubmit={handleSave}>
+          <table className="edit-mode-table">
             <tbody>
               <tr>
                 <td>Date:</td>
@@ -100,7 +116,12 @@ const SessionCard = ({ session, setSessions, getResponse }) => {
                   <input
                     type="date"
                     value={sessionDetails.session_date}
-                    onChange={(e) => setSessionDetails({ ...sessionDetails, session_date: formatDate(e.target.value) })}
+                    onChange={(e) =>
+                      setSessionDetails({
+                        ...sessionDetails,
+                        session_date: formatDate(e.target.value),
+                      })
+                    }
                     required
                   />
                 </td>
@@ -111,7 +132,12 @@ const SessionCard = ({ session, setSessions, getResponse }) => {
                   <input
                     type="time"
                     value={sessionDetails.session_time}
-                    onChange={(e) => setSessionDetails({ ...sessionDetails, session_time: e.target.value })}
+                    onChange={(e) =>
+                      setSessionDetails({
+                        ...sessionDetails,
+                        session_time: e.target.value,
+                      })
+                    }
                   />
                 </td>
               </tr>
@@ -121,7 +147,12 @@ const SessionCard = ({ session, setSessions, getResponse }) => {
                   <input
                     type="number"
                     value={sessionDetails.session_duration}
-                    onChange={(e) => setSessionDetails({ ...sessionDetails, session_duration: e.target.value })}
+                    onChange={(e) =>
+                      setSessionDetails({
+                        ...sessionDetails,
+                        session_duration: e.target.value,
+                      })
+                    }
                   />
                 </td>
               </tr>
@@ -131,7 +162,12 @@ const SessionCard = ({ session, setSessions, getResponse }) => {
                   <input
                     type="text"
                     value={sessionDetails.session_type}
-                    onChange={(e) => setSessionDetails({ ...sessionDetails, session_type: e.target.value })}
+                    onChange={(e) =>
+                      setSessionDetails({
+                        ...sessionDetails,
+                        session_type: e.target.value,
+                      })
+                    }
                   />
                 </td>
               </tr>
@@ -141,7 +177,12 @@ const SessionCard = ({ session, setSessions, getResponse }) => {
                   <input
                     type="number"
                     value={sessionDetails.session_fee}
-                    onChange={(e) => setSessionDetails({ ...sessionDetails, session_fee: e.target.value })}
+                    onChange={(e) =>
+                      setSessionDetails({
+                        ...sessionDetails,
+                        session_fee: e.target.value,
+                      })
+                    }
                   />
                 </td>
               </tr>
@@ -151,7 +192,12 @@ const SessionCard = ({ session, setSessions, getResponse }) => {
                   <input
                     type="text"
                     value={sessionDetails.session_status}
-                    onChange={(e) => setSessionDetails({ ...sessionDetails, session_status: e.target.value })}
+                    onChange={(e) =>
+                      setSessionDetails({
+                        ...sessionDetails,
+                        session_status: e.target.value,
+                      })
+                    }
                   />
                 </td>
               </tr>
@@ -161,16 +207,19 @@ const SessionCard = ({ session, setSessions, getResponse }) => {
                   <input
                     type="number"
                     value={sessionDetails.session_available_slots}
-                    onChange={(e) => setSessionDetails({ ...sessionDetails, session_available_slots: e.target.value })}
+                    onChange={(e) =>
+                      setSessionDetails({
+                        ...sessionDetails,
+                        session_available_slots: e.target.value,
+                      })
+                    }
                   />
                 </td>
               </tr>
             </tbody>
           </table>
           <div className="edit-mode-bottom">
-            <button onClick={handleSave} type="submit">
-              Save
-            </button>
+            <button onClick={handleSave}>Save</button>
             <button type="button" onClick={handleCancel}>
               Cancel
             </button>
