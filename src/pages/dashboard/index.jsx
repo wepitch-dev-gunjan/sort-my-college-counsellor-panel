4b76eb61ad5299d0c4ea744cd912122e1b38f6c7
Nikhil mandia <nikhilmandia7@gmail.com>
2024-04-10 16:49:09 +0530

    summary compoonent remove

--- src/pages/dashboard/index.jsx
+++ src/pages/dashboard/index.jsx
@@ -81,7 +81,7 @@ const Dashboard = () => {
           <Payment />
         </div>
       </div>
-      <div className="summary">{!smallScreen && <Summary />}</div>
+      {/* <div className="summary">{!smallScreen && <Summary />}</div> */}
     </div>
   );
 };
